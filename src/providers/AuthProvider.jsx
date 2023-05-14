9cc35cb2644548ac0255e0018fc5aa8da2893adb
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-15 03:59:13 +0600

    created auth context setup

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    const authInfo = {
+        user,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
