38336596ddc771f054a5370d5d6a91f5536a1b40
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-15 03:09:23 +0600

    added login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,46 @@
+import img from '../../assets/images/login/login.svg';
+
+const Login = () => {
+
+    const handleLogin = event =>{
+        event.preventDefault();
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className=" w-1/2 mr-12">
+                    <img src={img} alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl text-center font-bold">Login now!</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+
+                                <input type="submit" value="login" className="btn bg-orange-600" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
