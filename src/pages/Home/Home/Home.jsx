497da24e5ab4ac2ded760f6e9cd5e4b1239b8da6
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-15 02:43:36 +0600

    displayed service card from fake data

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
+            <Services></Services>
             <h2>THis is home</h2>
         </div>
     );
---
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,21 @@
+
+
+const ServiceCard = ({service}) => {
+    const {service_id, title, img, price, description} = service
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{title} </h2>
+                <p className="text-xl text-orange-600">Price: ${price}</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
