1289c39ae2bdd609ec9b87d9689014b7c2303caa
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-16 00:14:57 +0600

    fetched checkOuts data and added alert when insertedId is available

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,40 +1,79 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 const CheckOut = () => {
 
     const service = useLoaderData();
-    const { title, _id } = service;
+    const { title, _id, price, img } = service;
+    const {user} = useContext(AuthContext);
+
+    const handleCheckOut =  event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = user?.email;
+        const order = {
+            customerName: name,
+            email,
+            img,
+            date,
+            service: title,
+            service_id: _id,
+            price: price
+        }
+
+        console.log(order);
+
+        fetch('http://localhost:5000/checkOuts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('service book successfully')
+            }
+        })
+        
+    }
 
     return (
         <div>
-            <h2>Book service:{title} </h2>
+            <h2 className="text-center text-3xl">Book service:{title} </h2>
 
-            <form >
+            <form onSubmit={handleCheckOut}>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="form-control">
                         <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" defaultValue={user?.displayName}  className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <input type="date" name="date" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
+                        <input type="text" name="email" defaultValue={user?.email} placeholder="email" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text">Due Amount</span>
                         </label>
-                        <input type="text" placeholder="password" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <input type="text" defaultValue={'$'+ price} className="input input-bordered" />
                     </div>
                 </div>
 
