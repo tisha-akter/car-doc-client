71e2ee352684c7fea88d78a6a4ca52e01ebcb891
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-16 01:55:22 +0600

    displayed bookings and service img in a tabular format

---
+++ src/pages/Bookings/BookingRow.jsx
@@ -0,0 +1,35 @@
+
+
+const BookingRow = ({ booking }) => {
+    const {img, date, service, price } = booking;
+
+    return (
+        <tr>
+            <th>
+                <label>
+                    <input type="checkbox" className="checkbox" />
+                </label>
+            </th>
+            <td>
+
+                <div className="avatar">
+                    <div className="rounded w-24 h-24">
+                        { img &&  <img src={img} alt="Avatar Tailwind CSS Component" />}
+                    </div>
+                </div>
+
+
+            </td>
+            <td>
+                {service}
+            </td>
+            <td>{date}</td>
+            <td>${price}</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
+    );
+};
+
+export default BookingRow;
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,21 +1,54 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import BookingRow from "./BookingRow";
 
 const Bookings = () => {
 
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
 
     const url = `http://localhost:5000/checkOuts?email=${user?.email}`;
-    useEffect( () => {
+    useEffect(() => {
         fetch(url)
-        .then(res => res.json())
-        .then(data =>setBookings(data))
+            .then(res => res.json())
+            .then(data => setBookings(data))
     }, [])
 
     return (
         <div>
             <h2 className="text-5xl">Your Bookings: {bookings.length}</h2>
+
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Picture</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                       {
+                         bookings.map(booking => <BookingRow
+                            key={booking._id}
+                            booking={booking}
+                         ></BookingRow>)
+                       }
+                       
+                    </tbody>
+                 
+
+                </table>
+            </div>
 
         </div>
     );
