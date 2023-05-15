3f18427ae8bf177577a4719628fc9e0af3430a7d
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-15 17:17:49 +0600

    loaded services data from database

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
 
-const ServiceCard = ({service}) => {
-    const {service_id, title, img, price, description} = service
+const ServiceCard = ({ service }) => {
+    const { _id, title, img, price } = service
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -11,7 +12,9 @@ const ServiceCard = ({service}) => {
                 <h2 className="card-title">{title} </h2>
                 <p className="text-xl text-orange-600">Price: ${price}</p>
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <Link to={`/checkout/${_id}`}>
+                        <button className="btn btn-primary">Book Now</button>
+                    </Link>
                 </div>
             </div>
         </div>
